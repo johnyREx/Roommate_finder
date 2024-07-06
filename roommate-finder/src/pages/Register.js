004734a59d2758b0ac4ef3f61f8360e1b8b07e1d
johnyREx <johnkwofie99@icloud.com>
2024-07-06 10:50:08 +0200

    making of pages

---
+++ roommate-finder/src/pages/Register.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import RegisterComponent from '../components/RegisterComponent'
+
+
+const Register = () => {
+  return (
+    <div>
+        
+        <RegisterComponent />
+    </div>
+  )
+}
+
+export default Register
